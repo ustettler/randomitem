631e6ad5a4ba0fd76a86e638b0d3f45d8a3c2e79
Urs Stettler <62382591+ustettler@users.noreply.github.com>
2021-03-29 17:00:50 +0200

    New Code

--- script.js
+++ script.js
@@ -1,35 +1,103 @@
 console.log("Random")
 
+
 // Function RandomItem
-function random(){
-    //console.log("RandomItem")
-
-    let divs = document.getElementById('wrapper').getElementsByTagName('div');
-
-    // Loop durch alles Divs
-    for(let i = 0, len = divs.length; i < len; i++){
-        let div = divs[i];
-
-        //Variable Item
-        let item = +div.getElementsByClassName('item')[0].value;
-
-        console.log("item : " + item);
-
-        // Variable Random
-        //let randomItem = Math.floor(Math.random() * item);
+var teilnehmerliste = [];
+knopf.addEventListener("click", function(){
+		// feld holen
+    var eingabefeld = document.getElementById("teilnehmer");
+    var wert = eingabefeld.value;
+    // feld leeren
+    eingabefeld.value = "";
+    // wenn wert nicht leer, in liste eintragen!
+    if (wert != "") {
+    	 teilnehmerliste.push(wert);
     }
+    // liste darstellen
+    stelleListedar();
+});
 
-    // Radom Item im Dom Anzeigen
-        document.getElementById('randomItem').innerHTML = radomItem ;
+
+var loeschenknopf = document.getElementById("loeschen");
+loeschenknopf.addEventListener("click", function(){
+    listeLeeren();
+});
+
+function listeLeeren() {
+  teilnehmerliste = [];
+  sichtbareListeLeeren();
+}
+
+function sichtbareListeLeeren () {
+	var guiTeilnehmerliste = document.getElementById("teilnehmerliste");
+  while (guiTeilnehmerliste.firstChild) {
+      guiTeilnehmerliste.removeChild(guiTeilnehmerliste.firstChild);
+  }
+}
+
+function loescheEintrag (index) {
+	teilnehmerliste.splice(index, 1);
+  stelleListedar();
+}
+
+function stelleListedar() {
+	 // ul liste holen
+    var guiTeilnehmerliste = document.getElementById("teilnehmerliste");
+    // ul liste leeren
+    while (guiTeilnehmerliste.firstChild) {
+      guiTeilnehmerliste.removeChild(guiTeilnehmerliste.firstChild);
+  	}
+    // ul liste füllen
+    for (var i=0; i < teilnehmerliste.length; i++) {
+  		var li = document.createElement("li");
+  		var textKnoten = document.createTextNode(teilnehmerliste[i]);
+  	 	li.appendChild(textKnoten);
+      // remove x an li hängen
+      var span = document.createElement("span");
+      var txt = document.createTextNode("x");
+      span.className = "delete";
+      span.appendChild(txt);
+      li.appendChild(span);
+      // li mit data index ausstatten
+      li.setAttribute('data-array-index', i);
+      // li in gui platzieren
+      guiTeilnehmerliste.appendChild(li);
+  	}
+    
+    // deletes funktion anhängen
+    var deletes = document.getElementsByClassName("delete");
+  	for (var j = 0; j < deletes .length; j++) {
+    	deletes [j].onclick = function() {
+    	var eintrag = this.parentElement;  // auf das LI zugreifen
+    	var referenz = eintrag.getAttribute('data-array-index');
+    	loescheEintrag(referenz);
+    	};
+    }
 }
 
 
+var ermittelnKnopf = document.getElementById("ermitteln");
+ermittelnKnopf.addEventListener("click", function(){
+    var anzahlGewinner = document.getElementById("anzahlGewinner");
+    var wert = anzahlGewinner.value;
+    var gewinnerString = "";
+		for (var i = 0; i < wert; i++) {
+    	  var gewinner = giveRandomItem(teilnehmerliste);
+    		gewinnerString += gewinner;
+    }
+    window.alert("Die oder der Gewinner ist: " + gewinnerString + "");
+    console.log(gewinnerString);
+});
+
+function giveRandomItem (array) {
+ return array[Math.floor(Math.random() * array.length)];
+}
 
 
    // Zufallszahl
     document.querySelector('#submit').addEventListener("click", ausgabeZufallszahl);
 
-	let randomNumberMin = 000000000000;
+	let randomNumberMin = 000000;
 		randomNumberMax = 999999999999;
 
 
